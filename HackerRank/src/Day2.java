ebd138b2a3aeec02f26517eaa2f72006b47fe016
JayB Kim <jaybkim2@gmail.com>
2017-09-28 11:10:51 +0900

    Day 2

--- HackerRank/src/Day2.java
+++ HackerRank/src/Day2.java
@@ -11,9 +11,11 @@ public class Day2 {
         scan.close();
       
         // Write your calculation code here.
-      
+        double tip = mealCost * tipPercent / 100;
+        double tax = mealCost * taxPercent / 100;
+        
         // cast the result of the rounding operation to an int and save it as totalCost 
-        int totalCost = (int) Math.round(mealCost + (mealCost * tipPercent / 100) + (mealCost * taxPercent / 100));
+        int totalCost = (int) Math.round(mealCost + tip + tax);
       
         // Print your result
         System.out.println("The total meal cost is " + totalCost + " dollars.");
