58a6d979e6470eb22a7d7a864d8939f0677dae02
JayB Kim <jaybkim2@gmail.com>
2017-09-29 13:01:33 +0900

    Day 3

--- HackerRank/src/Day1.java
+++ HackerRank/src/Day1.java
@@ -9,15 +9,22 @@ public class Day1 {
         double d = 4.0;
         String s = "HackerRank ";
 		
+        //Scanner scan = new Scanner(System.in).useDelimiter("\\n");
         Scanner scan = new Scanner(System.in);
-
+        
         /* Declare second integer, double, and String variables. */
-        int i2 = 12;
-        double d2 = 4.0;
-        String s2 = "is the best place to learn and practice coding!";
+        int i2;
+        double d2;
+        String s2;
        
         /* Read and save an integer, double, and String to your variables.*/
         // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
+        i2 = scan.nextInt();
+        d2 = scan.nextDouble();
+        
+        // Important!
+        scan.nextLine(); // read the rest of the line of input (newline character after the double token). 
+    	s2 = scan.nextLine();	
         
         /* Print the sum of both integer variables on a new line. */
         System.out.println(i  + i2);
@@ -27,8 +34,9 @@ public class Day1 {
        
         /* Concatenate and print the String variables on a new line; 
         	the 's' variable above should be printed first. */
-        System.out.println(s + s2); 
-		
+        System.out.println(s + s2);
+        
+        scan.close();
 	}
 
 }
