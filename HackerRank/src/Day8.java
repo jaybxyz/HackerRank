0922139edbe1e1395986e472ffb7bd6dbc31baad
JayB Kim <jaybkim2@gmail.com>
2017-10-04 12:25:25 +0900

    Day 8

---
+++ HackerRank/src/Day8.java
@@ -0,0 +1,41 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+
+public class Day8 {
+
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        
+        in.nextLine();
+        
+        // Create a HashMap that contains name and phone as key / value. 
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        
+        for(int i = 0; i < n; i++){
+            String name = in.next();
+            int phone = in.nextInt();
+            
+            // Write code here
+            in.nextLine();
+            map.put(name, phone);
+        }
+        
+        while(in.hasNext()){
+            String s = in.next();
+            
+            // Write code here
+            if (map.get(s) == null) {
+				System.out.println("Not found");
+			}else{
+				System.out.println(s + "=" + map.get(s));
+			}
+        }
+        in.close();
+		
+	}
+
+}
